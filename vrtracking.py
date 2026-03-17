c7f75d053121c066886ddbc94fd90a4c4b7d3791
Joshwott <joshwotbrown@btinternet.com>
2026-03-17 16:15:27 +0000

    Code for tracking controller and headset positioning.
    Removed redundant file from code.

--- vrtracking.py
+++ vrtracking.py
@@ -55,3 +55,35 @@ except openvr.OpenVRError as e:
 
 finally:
     openvr.shutdown()
+
+
+'''
+try:
+    vr_system = openvr.init(openvr.VRApplication_Scene)
+
+    print("Scanning for connected VR devices...\n")
+
+    for device_index in range(openvr.k_unMaxTrackedDeviceCount):
+
+        if not vr_system.isTrackedDeviceConnected(device_index):
+            continue
+
+        device_class = vr_system.getTrackedDeviceClass(device_index)
+        device_type = get_device_class(device_class)
+
+        model = vr_system.getStringTrackedDeviceProperty(
+            device_index,
+            openvr.Prop_ModelNumber_String
+        )
+
+        print("Device ID:", device_index)
+        print("Type:", device_type)
+        print("Model:", model)
+        print("----------------------")
+
+    openvr.shutdown()
+
+except openvr.OpenVRError as e:
+    print("SteamVR not detected or headset not connected.")
+    print(e)
+'''
