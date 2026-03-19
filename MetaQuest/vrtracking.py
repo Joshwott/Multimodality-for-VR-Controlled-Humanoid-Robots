7d88c9ea234549edb64245907bd39152587e559c
Joshwott_Laptop <joshwotbrown@btinternet.com>
2026-03-19 21:50:08 +0000

    Changed approach to avoid needing a VR Ready machine by receiving contorller data from a webXR Page transferred across using a websocket in the form of JSON data.

--- MetaQuest/vrtracking.py
+++ MetaQuest/vrtracking.py
@@ -1,58 +1,23 @@
-import openvr
-import time
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+import json
 
-def get_device_class(device_class):
-    if device_class == openvr.TrackedDeviceClass_HMD:
-        return "Headset"
-    elif device_class == openvr.TrackedDeviceClass_Controller:
-        return "Controller"
-    elif device_class == openvr.TrackedDeviceClass_TrackingReference:
-        return "Base Station"
-    elif device_class == openvr.TrackedDeviceClass_GenericTracker:
-        return "Tracker"
-    else:
-        return "Unknown"
+class ControllerDataServer(WebSocket):
+    def receiveMessage(self):
+        try:
+            data = json.loads(self.data)
+            print("Controller: ", data['controller'])
+            print("  Position: ", data['position'])
+            print("  Orientation:", data['orientation'])
+        except ValueError:
+            print("Invalid JSON Received: ", self.data)
 
-def get_position(pose):
-    matrix = pose.mDeviceToAbsoluteTracking
-    return matrix[0][3], matrix[1][3], matrix[2][3]
+    def connected(self):
+        print(self.address, "Connected")
 
+    def disconnected(self):
+        print(self.address, "Disconnected")
 
-def start_tracking():
-    try:
-        vr_system = openvr.init(openvr.VRApplication_Scene)
-        while True:
-
-            poses = vr_system.getDeviceToAbsoluteTrackingPose(openvr.TrackingUniverseStanding, 0,
-                                                              openvr.k_unMaxTrackedDeviceCount)
-
-            for i in range(openvr.k_unMaxTrackedDeviceCount):
-
-                if not vr_system.isTrackedDeviceConnected(i):
-                    continue
-
-                device_class = vr_system.getTrackedDeviceClass(i)
-
-                if device_class != vr_system.getTrackedDeviceClass(i):
-                    continue
-
-                pose = poses[i]
-
-                if not pose.bPoseIsValid:
-                    continue
-
-                position = get_position(pose)
-                print("Device ID:", i)
-                print("Position (X, Y, Z:", position)
-                print("----------------------")
-
-            time.sleep(0.1)
-
-    except KeyboardInterrupt:
-        print("Stopped tracking.")
-
-    except openvr.OpenVRError as e:
-        print("SteamVR error:", e)
-
-    finally:
-        openvr.shutdown()
+def startServer():
+        server = SimpleWebSocketServer('0.0.0.0', 8000, ControllerDataServer)
+        print("Server Started")
+        server.serveforever()
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Main file responsible for initiating all the code.
 
 from MetaQuest import vrtracking
+'''
 from NAORobot import naocamera, naocontrols, naotest
+'''
 
-# Main method starts the Meta Qust 2 and NAO robot links
+# Main method starts the Meta Quest 2 and NAO robot links
 def main():
-    vrtracking.start_tracking()
+    vrtracking.startServer()
 
 if __name__ == "__main__":
     main()
